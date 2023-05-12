1b5e93f0c0401eabf4d9f93a7773739f09252088
fchntst <91413895+fchntst@users.noreply.github.com>
2023-05-12 23:53:42 +0200

    Add files via upload

---
+++ program5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void hello(){
+    printf("hello world! ");
+}
+
+int main()
+{
+    hello(); hello();
+    hello();
+    hello();
+    return 0;
+}
---
+++ program6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void hello()
+{
+    printf("Hello World!\n");
+}
+
+int main()
+{
+    for (int i=0; i<=12; i++)
+    hello();
+    return 0;
+}
