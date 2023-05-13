c18db858eaada8e2870b5bebf66c6019491da872
fchntst <91413895+fchntst@users.noreply.github.com>
2023-05-13 15:57:46 +0200

    Create print1.c

---
+++ Printing/print1.c
@@ -0,0 +1,16 @@
+// Nice program
+#include <stdio.h>
+
+int main()
+{
+    printf("\n\t\tANAFORA");
+    printf("\n -----------------------------------");
+    printf("\n\tAtomo\t\tAmoibi ");
+    printf("\n -----------------------------------");
+    printf("\n\tGiorgos\t\t100 ");
+    printf("\n\tKostas\t\t200 ");
+    printf("\n\tEleni\t\t150 ");
+    printf("\n -----------------------------------");
+
+    printf("\n\aBeep.\n\aBeep.\n\aBeep.");
+}
